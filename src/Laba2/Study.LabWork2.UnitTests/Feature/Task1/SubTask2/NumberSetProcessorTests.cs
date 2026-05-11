8b361a2138d153dd064f87f703ade1f4c97b8449
Екатерина <@WIN-442LET13K9F>
2026-05-11 21:33:48 +0300

    Выполнено Task1 SubTask2 и соответвующий юнит-тест

--- src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask2/NumberSetProcessorTests.cs
+++ src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask2/NumberSetProcessorTests.cs
@@ -1,6 +1,80 @@
 namespace Study.LabWork2.UnitTests.Feature.Task1.SubTask2;
 
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+using Study.LabWork2.Feature.Task1.SubTask2;
+
 [TestFixture]
-public sealed class NumberSetProcessorTests
+public class NumberSetProcessorTests
 {
+    private string _testFilePath;
+
+    [SetUp]
+    public void Setup()
+    {
+        _testFilePath = "test_numbers.txt";
+
+        NumberSetProcessor.GenerateDataFile(_testFilePath);
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        if (File.Exists(_testFilePath))
+        {
+            File.Delete(_testFilePath);
+        }
+    }
+
+    [Test]
+    public void GenerateDataFile_Creates15Sets()
+    {
+        string[] lines = File.ReadAllLines(_testFilePath);
+
+        Assert.That(lines.Length, Is.EqualTo(15));
+    }
+
+    [Test]
+    public void EachSet_Contains100Numbers()
+    {
+        string[] lines = File.ReadAllLines(_testFilePath);
+
+        foreach (string line in lines)
+        {
+            int count = line
+                .Split(' ', System.StringSplitOptions.RemoveEmptyEntries)
+                .Length;
+
+            Assert.That(count, Is.EqualTo(100));
+        }
+    }
+
+    [Test]
+    public void ProcessSets_CalculatesTotalSumCorrectly()
+    {
+        NumberSetProcessor processor =
+            new NumberSetProcessor(_testFilePath, 3);
+
+        processor.ProcessSets();
+
+        int expectedSum = File.ReadAllLines(_testFilePath)
+            .SelectMany(line =>
+                line.Split(' ', System.StringSplitOptions.RemoveEmptyEntries))
+            .Select(int.Parse)
+            .Sum();
+
+        Assert.That(processor.TotalSum, Is.EqualTo(expectedSum));
+    }
+
+    [Test]
+    public void ProcessSets_Produces15Results()
+    {
+        NumberSetProcessor processor =
+            new NumberSetProcessor(_testFilePath, 3);
+
+        processor.ProcessSets();
+
+        Assert.That(processor.Results.Count, Is.EqualTo(15));
+    }
 }
--- src/Laba2/Study.LabWork2/Program.cs
+++ src/Laba2/Study.LabWork2/Program.cs
@@ -1,9 +1,40 @@
+using Study.LabWork2.Feature.Task1.SubTask2;
+using System;
+using System.IO;
+
 namespace Study.LabWork2;
 
 public static class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        // todo: какая-то обертка для вызова классов заданий
+        string filePath = "numbers.txt";
+
+        if (!File.Exists(filePath))
+        {
+            NumberSetProcessor.GenerateDataFile(filePath);
+
+            Console.WriteLine("Файл с наборами чисел создан.\n");
+        }
+
+        int maxThreads = 3;
+
+        NumberSetProcessor processor =
+            new NumberSetProcessor(filePath, maxThreads);
+
+        processor.ProcessSets();
+
+        Console.WriteLine("Результаты обработки:\n");
+
+        foreach (string result in processor.Results)
+        {
+            Console.WriteLine(result);
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Общий итог: {processor.TotalSum}");
+
+        Console.WriteLine($"Время выполнения: {processor.ExecutionTimeMs} мс");
     }
 }
