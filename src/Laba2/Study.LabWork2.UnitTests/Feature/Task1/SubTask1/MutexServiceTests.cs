13164f3c17846531f78c8ba62c769115f68ea5c5
aeskatery <110762530+aeskatery@users.noreply.github.com>
2026-05-20 23:48:19 +0300

    Выполнено Task1 SubTask1 и соответсвующие юнит-тесты (#7)
    Co-authored-by: Екатерина <Екатерина@WIN-442LET13K9F>

--- src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/MutexServiceTests.cs
+++ src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/MutexServiceTests.cs
@@ -1,6 +1,25 @@
+using NUnit.Framework;
+using Study.LabWork2.Feature.Task1.SubTask1;
+
 namespace Study.LabWork2.UnitTests.Feature.Task1.SubTask1;
 
 [TestFixture]
-public sealed class MutexServiceTests
+public class MutexServiceTests
 {
+    [Test]
+    [TestCase(2, true)]
+    [TestCase(3, true)]
+    [TestCase(5, true)]
+    [TestCase(17, true)]
+    [TestCase(97, true)]
+    [TestCase(113, true)]
+    [TestCase(1, false)]
+    [TestCase(4, false)]
+    [TestCase(100, false)]
+    [TestCase(0, false)]
+    public void IsPrime_ReturnsCorrectResult(int number, bool expected)
+    {
+        bool actual = MonitorService.IsPrime(number);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
